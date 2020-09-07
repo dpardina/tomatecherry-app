cd3de689d93ade0799c6a6358c3c426c93dcc351
dpardina <diego.pardina@gmail.com>
2020-09-07 17:38:04 -0300

    Actualizado

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,7 @@
+import chocotorta from '../assets/img/chocotortabrownie.jpg';
+
 const products = [
-    {id: '1', name: 'Chocotorta Brownie', price: 1170, stock: 5},
+    {id: '1', name: 'Chocotorta Brownie', price: 1170, stock: 5, image: {chocotorta}},
     {id: '2', name: 'Mix Brownies', price: 590, stock: 10},
     {id: '3', name: 'Cherrymix', price: 700, stock: 10}
 ];
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -8,9 +8,14 @@ import cherrymix from '../assets/img/cherrymix.jpg';
 export default function ItemList() {
     return (
         <div className="carousel row">
-            {products.map((id, name, price, stock) => (
-                <div id={id} className="card text-center col-12 col-md-4 mb-5">
-                    {name}
+            {products.map(element => (
+                <div id={element.id} className="card text-center col-12 col-md-4 mb-5">
+                    <img src={element.image} className="card-img-top" alt={element.name} />
+                    <h4 className="card-title p-2">{element.name}</h4>
+                    <div className="d-flex flex-wrap justify-content-around align-items-center p-2 torta-precio">
+                        <h5 className="card-text">${element.price} por Unidad</h5>
+                        <ItemCount initial="0" min="0" max={element.stock} />
+                    </div>
                 </div>
             ))}
         </div>
