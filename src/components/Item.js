44dcd17aafa59e062500d8f258ce5e9eb1a60826
dpardina <diego.pardina@gmail.com>
2020-09-08 13:45:40 -0300

    Actualizado

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,10 +2,16 @@ import chocotorta from '../assets/img/chocotortabrownie.jpg';
 import mixbrownie from '../assets/img/mix-brownie.jpg';
 import cherrymix from '../assets/img/cherrymix.jpg';
 
-const products = [
-    {id: '1', name: 'Chocotorta Brownie', price: 1170, stock: 5, image: chocotorta},
-    {id: '2', name: 'Mix Brownies', price: 590, stock: 10, image: mixbrownie},
-    {id: '3', name: 'Cherrymix', price: 700, stock: 10, image: cherrymix}
-];
+const task = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        const products = [
+            {id: '1', name: 'Chocotorta Brownie', price: 1170, stock: 5, image: chocotorta},
+            {id: '2', name: 'Mix Brownies', price: 590, stock: 10, image: mixbrownie},
+            {id: '3', name: 'Cherrymix', price: 700, stock: 10, image: cherrymix}
+        ]
+        resolve(products);
+    }, 2000);
+});
 
 export default products;
