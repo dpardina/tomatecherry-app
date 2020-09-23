b9639593d16aef2eefa047a9104287f89b79d848
dpardina <diego.pardina@gmail.com>
2020-09-23 15:51:40 -0300

    Update

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Item = ({ product }) => {
   const { id, name, price, measure, image } = product;
 
   return (
     <React.Fragment>
-      <Link to={{pathname: `/torta/${id}`, state: {test:'test'}}}>
-      <img src={image} className="card-img-top" alt={name} />
-      </Link>
+      <NavLink to={{pathname: `/torta/${id}`, state: {test:'test'}}}>
+        <img src={image} className="card-img-top" alt={name} />
+      </NavLink>
       <h4 className="card-title p-2">{name}</h4>
       <h5 className="torta-precio p-2 mb-0">${price} por {measure}</h5>
     </React.Fragment>
